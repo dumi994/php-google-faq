50fdf491a7483dc589bffe318bfc9d0bb134c8dd
dumi994 <dumi.9944@gmail.com>
2021-06-23 18:51:56 +0200

    aggiunto il link active nella navbar e links nel contenuto

--- index.php
+++ index.php
@@ -5,7 +5,8 @@ $faqs = [
         "Come state implementando la recente decisione della Corte di giustizia dell'Unione europea (CGUE) relativa al diritto all'oblio?" => 
     [
         "<p>
-            La recente decisione della Corte di giustizia dell'Unione europea ha profonde conseguenze per i motori di ricerca in Europa. 
+
+            La recente <a href=''>decisione della Corte di giustizia dell'Unione</a>  europea ha profonde conseguenze per i motori di ricerca in Europa. 
             La Corte ha stabilito che alcuni utenti hanno il diritto di chiedere ai motori di ricerca come Google di rimuovere risultati relativi a chiavi di ricerca che includono il proprio nome. 
             Per poter essere rimossi, i risultati visualizzati devono essere inadeguati, irrilevanti o non più rilevanti, o eccessivi.
         </p
@@ -16,7 +17,8 @@ $faqs = [
         </p>
         
         <p>
-            Per presentare una richiesta di rimozione, compila questo modulo web. Riceverai una risposta automatica che conferma la ricezione della tua richiesta. 
+
+            Per presentare una richiesta di rimozione, compila <a href=''>questo modulo web</a>. Riceverai una risposta automatica che conferma la ricezione della tua richiesta. 
             Dopodiché valuteremo il tuo caso (tieni presente che potrebbe occorrere un po' di tempo perché abbiamo già ricevuto tante richieste simili). 
             Nel valutare la richiesta, considereremo se i risultati includono informazioni obsolete relative alla tua vita privata. 
             Considereremo inoltre se vi è un interesse pubblico a che le informazioni rimangano nei nostri risultati delle ricerche, ad esempio se riguardano frodi finanziarie, negligenza professionale, 
@@ -49,10 +51,11 @@ $faqs = [
             Abbiamo inoltre sviluppato strumenti per la sicurezza e la privacy di facile utilizzo come Google Dashboard, la verifica in due passaggi e Impostazioni annunci. 
             Così, per quanto riguarda le informazioni che condividi con Google, hai il massimo controllo.
         </p>
+
         
         <p>
-            Puoi leggere ulteriori informazioni sulla sicurezza online, incluso come proteggere te e la tua famiglia online, nel Centro Google per la sicurezza online.
-            Scopri quali misure adottiamo per garantire protezione e sicurezza alle tue informazioni personali, lasciando a te il controllo.
+            Puoi leggere ulteriori informazioni sulla sicurezza online, incluso come proteggere te e la tua famiglia online, nel <a href=''>Centro Google per la sicurezza online</a>.
+            <a href=''>Scopri</a> quali misure adottiamo per garantire protezione e sicurezza alle tue informazioni personali, lasciando a te il controllo.
         </p>"
         
     ],
@@ -104,20 +107,21 @@ $faqs = [
      [
         "I risultati di ricerca di Google rispecchiano i contenuti pubblicamente disponibili sul Web.
         I motori di ricerca non possono rimuovere i contenuti direttamente dai siti web, quindi rimuovere risultati di ricerca da Google non consente di rimuovere i contenuti dal Web.
-        Se desideri rimuovere qualcosa dal Web, devi contattare il webmaster del sito su cui sono pubblicati i contenuti e chiedergli di apportare una modifica. 
-        Inoltre, se, ai sensi delle leggi europee per la protezione dei dati, desideri richiedere la rimozione di determinate informazioni su di te visualizzate nei risultati di ricerca di Google, fai clic qui.
+
+        Se desideri rimuovere qualcosa dal Web, devi <a href=''>contattare il webmaster</a> del sito su cui sono pubblicati i contenuti e chiedergli di apportare una modifica. 
+        Inoltre, se, ai sensi delle leggi europee per la protezione dei dati, desideri richiedere la rimozione di determinate informazioni su di te visualizzate nei risultati di ricerca di Google, <a href=''>fai clic qui</a>.
         Una volta che i contenuti saranno stati rimossi e che Google avrà rilevato l'aggiornamento, le informazioni non verranno più visualizzate nei risultati di ricerca di Google.
         In caso di una richiesta di rimozione urgente,
-         è inoltre possibile visitare la nostra pagina di assistenza per avere ulteriori informazioni."
+         è inoltre possibile <a href=''>visitare la nostra pagina di assistenza per avere ulteriori informazioni.</a>"
     ],
     //faq 6
         "Quando faccio clic sui risultati della Ricerca Google, le mie chiavi di ricerca vengono inviate ai siti web?" => 
     [
-        "In alcuni casi sì. Quando fai clic su un risultato della Ricerca Google, il tuo browser web potrebbe reindirizzare alla pagina web di destinazione anche l'indirizzo Internet, o URL, della pagina dei risultati di ricerca sotto forma di URL referrer.
+        "In alcuni casi sì. Quando fai clic su un risultato della Ricerca Google, il tuo browser web potrebbe reindirizzare alla pagina web di destinazione anche l'indirizzo Internet, o URL, della pagina dei risultati di ricerca sotto forma di <a href=''>URL referrer</a>.
         Talvolta, l'URL della pagina dei risultati di ricerca potrebbe contenere la query di ricerca che hai inserito. 
         Se utilizzi la ricerca SSL (la funzione di ricerca criptata di Google), nella maggior parte dei casi i termini di ricerca non vengono inviati come parte dell'URL negli URL referrer. 
         Questo comportamento può fare eccezione, ad esempio se utilizzi alcuni browser meno diffusi. 
-        Ulteriori informazioni sulla ricerca SSL sono disponibili qui. 
+        Ulteriori informazioni sulla ricerca SSL sono disponibili <a href=''>qui</a> . 
         Le query di ricerca o le informazioni contenute nell'URL referrer potrebbero essere disponibili mediante Google Analytics o un'API (Application Programming Interface). 
         Inoltre, gli inserzionisti potrebbero ricevere informazioni relative all' esatte parole chiave che hanno determinato il clic su un annuncio."
     ],
@@ -161,7 +165,7 @@ $faqs = [
         <a href="">Norme sulla privacy</a>
         <a href="">Termini di servizio</a>
         <a href="">Tecnologie</a>
-        <a href="">Domande frequenti</a>
+        <a href="" class="active">Domande frequenti</a>
 
     </div>
 </nav>
